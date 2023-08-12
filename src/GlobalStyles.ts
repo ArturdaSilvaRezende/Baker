667132899901ed734d140139c550cad8d4ef3125
Artur da Silva Rezende <artursrezende@gmail.com>
2023-08-12 18:30:42 -0300

    Initial setup completed

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,39 @@
+/* src/GlobalStyles.ts */
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        font-size: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    //font-family: 'Playfair Display', serif;
+
+    a {
+        text-decoration: none;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
+
+    :root {
+        --primary: #EAA636;
+        --secondary: #545454;
+        --light: #FDF5EB;
+        --dark: #1E1916;
+    }
+`;
+
+export default GlobalStyles;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Home // Routes</div>;
+};
+
+export default Home;
